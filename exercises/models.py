1922c3c327f7ffc0dcbc389580b8c14a26fc1da9
Robert Morgan <robert.morgan0703@gmail.com>
2022-06-09 12:05:13 +0100

    fixed project folder and added model serializer

--- exercises/models.py
+++ exercises/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Exercise(models.Model):
+    name = models.CharField(max_length=100, default=None)
+    grouping = models.CharField(max_length=100, default=None)
+    description = models.TextField(max_length=300, blank=True, null=True)
+    video_url = models.CharField(max_length=300, blank=True, null=True)
+    createdby = models.ForeignKey(
+      'jwt_auth.User',
+      related_name='exercises',
+      on_delete= models.CASCADE
+    )
