f115b6a0624f0af7d87e995f37f5f5a1572f0171
Robert Morgan <robert.morgan0703@gmail.com>
2022-06-09 11:42:12 +0100

    working login and register routes

--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -1,3 +1,4 @@
+import email
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -12,3 +13,47 @@ from .serializers.common import UserSerializer
 # User Model
 from django.contrib.auth import get_user_model
 User = get_user_model()
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        user_to_register = UserSerializer(data=request.data)
+        try:
+            user_to_register.is_valid()
+            print('validation errors =>', user_to_register.errors)
+            user_to_register.save()
+            return Response({ 'message': 'Registration Successful'}, status.HTTP_202_ACCEPTED)
+
+        except Exception as e:
+            print(e)
+            return Response({ 'detail': str(e) }, status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+class LoginView(APIView):
+
+    def post(self, request):
+
+        email = request.data.get('email')
+        password = request.data.get('password')
+        try:
+            user_to_register = User.objects.get(email=email)
+
+        # User doesn't exist
+        except User.DoesNotExist:
+            raise PermissionDenied('Invalid login details')
+
+        # Wrong Password
+        if not user_to_register.check_password(password):
+            raise PermissionDenied('Invalid login details')
+
+        # Building the token
+        dt = datetime.now() + timedelta(hours=3)
+        token = jwt.encode(
+            {
+                'sub': user_to_register.id,
+                'exp': int(dt.strftime('%s'))
+            },
+            settings.SECRET_KEY,
+            algorithm='HS256'
+        )
+        
+        return Response({ 'message': f"Welcome back, {user_to_register.username}", 'token': token }, status.HTTP_202_ACCEPTED)
