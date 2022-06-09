ca20fa5ff8066cba2b43c50c22dff6a2b68238c1
Robert Morgan <robert.morgan0703@gmail.com>
2022-06-09 11:58:38 +0100

    created model

---
+++ project/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=100, default=None)
+    grouping = models.CharField(max_length=100, default=None)
+    description = models.TextField(max_length=300, blank=True, null=True)
+    video_url = models.CharField(max_length=300, blank=True, null=True)
+    createdby = models.ForeignKey(
+      'jwt_auth.User',
+      related_name='exercises',
+      on_delete= models.CASCADE
+    )
