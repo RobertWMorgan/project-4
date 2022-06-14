e1404523fb0a0d67138c30ed8cb15304b2209b3f
Robert Morgan <robert.morgan0703@gmail.com>
2022-06-14 10:17:43 +0100

    sidebar highlights

--- client/src/components/common/sidebars/SidebarCollapsed.js
+++ client/src/components/common/sidebars/SidebarCollapsed.js
@@ -1,17 +1,57 @@
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { getUserName } from '../../../helpers/Auth'
 
 const SidebarCollapsed = () => {
+  const location = useLocation()
+  const [currentLocation, setCurrentLocation] = useState('/')
+
+  useEffect(() => {
+    setCurrentLocation(location.pathname)
+  }, [location])
+
+  useEffect(() => {
+    setCurrentLocation(location.pathname)
+    console.log(location.pathname)
+  }, [location])
   return (
-    <div className = 'sidebar-collapsed'>
-      <p>Login to view profile</p>
+    <div className='sidebar-collapsed'>
+      <p>Please Login</p>
       <ul className='nav_icons'>
-        <img className='nav_icon' src='/images/home-grey.png' alt='home'/>
-        <img className='nav_icon' src='/images/dumbell-grey.png' alt='dumbell'/>
-        <img className='nav_icon' src='/images/calendar-grey.png' alt='calendar'/>
-        <img className='nav_icon' src='/images/power-grey.png' alt='power-button'/>
+        {
+          (currentLocation === '/') ?
+            <>
+              <img className='nav_icon' src='/images/home-white.png' alt='home' />
+            </>
+            :
+            <>
+              <img className='nav_icon' src='/images/home-grey.png' alt='home' />
+            </>
+        }
+        {
+          (currentLocation === '/exercises') ?
+            <>
+              <img className='nav_icon' src='/images/dumbell-white.png' alt='dumbell' />
+            </>
+            :
+            <>
+              <img className='nav_icon' src='/images/dumbell-grey.png' alt='dumbell' />
+            </>
+        }
+        {
+          (currentLocation === `/calendar/${getUserName()}`) ?
+            <>
+              <img className='nav_icon' src='/images/calendar-white.png' alt='calendar' />
+            </>
+            :
+            <>
+              <img className='nav_icon' src='/images/calendar-grey.png' alt='calendar' />
+            </>
+        }
+        <img className='nav_icon' src='/images/power-grey.png' alt='power-button' />
       </ul>
     </div>
   )
 }
-
 
 export default SidebarCollapsed
--- client/src/components/common/sidebars/SidebarExpanded.js
+++ client/src/components/common/sidebars/SidebarExpanded.js
@@ -1,8 +1,16 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
+import react, { useState, useEffect } from 'react'
 import { getUserName } from '../../../helpers/Auth'
 
 const SidebarExpanded = () => {
   const navigate = useNavigate()
+  const location = useLocation()
+
+  const [currentLocation, setCurrentLocation] = useState('/')
+
+  useEffect(() => {
+    setCurrentLocation(location.pathname)
+  }, [location])
 
   const handleLogout = () => {
     window.localStorage.removeItem('brogress-username')
@@ -10,24 +18,57 @@ const SidebarExpanded = () => {
     navigate('/')
   }
   return (
-    <div className = 'sidebar-expanded'>
-      <p>Login to view profile</p>
+    <div className='sidebar-expanded'>
+      <p>Please Login</p>
       <ul className='nav_icons'>
         <span onClick={() => navigate('/')}>
-          <p>Home</p>
-          <img className='nav_icon' src='/images/home-grey.png' alt='home'/>
+          {
+            (currentLocation === '/') ?
+              <>
+                <p className='sidebar-white'>Home</p>
+                <img className='nav_icon' src='/images/home-white.png' alt='home' />
+              </>
+              :
+              <>
+                <p>Home</p>
+                <img className='nav_icon' src='/images/home-grey.png' alt='home' />
+              </>
+          }
         </span>
         <span onClick={() => navigate('/exercises')}>
-          <p>My Exercises</p>
-          <img className='nav_icon' src='/images/dumbell-grey.png' alt='dumbell'/>
+          {
+            (currentLocation === '/exercises') ?
+              <>
+                <p className='sidebar-white'>My Exercises</p>
+                <img className='nav_icon' src='/images/dumbell-white.png' alt='dumbell' />
+              </>
+              :
+              <>
+                <p>My Exercises</p>
+                <img className='nav_icon' src='/images/dumbell-grey.png' alt='dumbell' />
+              </>
+          }
+
+          
         </span>
         <span onClick={() => navigate(`/calendar/${getUserName()}`)}>
-          <p>Exercise Tracker</p>
-          <img className='nav_icon' src='/images/calendar-grey.png' alt='calendar'/>
+          {
+            (currentLocation === `/calendar/${getUserName()}`) ?
+              <>
+                <p className='sidebar-white'>Exercise Tracker</p>
+                <img className='nav_icon' src='/images/calendar-white.png' alt='calendar' />
+              </>
+              :
+              <>
+                <p>Exercise Tracker</p>
+                <img className='nav_icon' src='/images/calendar-grey.png' alt='calendar' />
+              </>
+          }
+
         </span>
         <span onClick={handleLogout}>
           <p>Sign Out</p>
-          <img className='nav_icon' src='/images/power-grey.png' alt='power-button'/>
+          <img className='nav_icon' src='/images/power-grey.png' alt='power-button' />
         </span>
       </ul>
     </div>
