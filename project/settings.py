3625748636a350e1ce1ef0e20388a22bf8607d8f
Robert Morgan <robert.morgan0703@gmail.com>
2022-06-23 15:20:04 +0100

    master

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#99f3rfwu8ja@5+p0)rctah8bcw4miu^clx=up^*!r92r#-^py'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
